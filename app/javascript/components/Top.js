a8bcee2855d2e979fe6c2b48473795ef6ef616d0
key2-hue <keymaeda3782@gmail.com>
2019-12-20 17:57:05 +0900

    12/20 17:57

--- app/javascript/components/Top.js
+++ app/javascript/components/Top.js
@@ -4,10 +4,23 @@ import PropTypes from "prop-types"
 
 
 class Top extends React.Component{
-   
+   constructor() {
+     super();
+     this.state = {
+       count: 0
+     }
+   }
+
+   countUp() {
+     this.setState(
+      {
+        count: this.state.count + 1
+      })
+   }
+
     render() {
       return (
-        <h1>Hello {this.props.name}!</h1>
+        <button className="countUp" onClick={() => {this.countUp()}}>{this.state.count}</button>
       )
     }
   }
