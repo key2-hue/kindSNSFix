41edfeb2a61426466ddd8b5c11b9959139c70ce8
key2-hue <keymaeda3782@gmail.com>
2020-02-08 17:38:47 +0900

    02/08 17:38

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -12,7 +12,7 @@ class TopController < ApplicationController
     @eachTopic = Top.find(params[:id]);
     @comment = Comment.new
     @top = TopUser.new;
-    @commentAll = Comment.where(top_id: @eachTopic).order(created_at: "ASC");
+    @commentAll = Comment.where(top_id: @eachTopic).page(params[:page]).per(10).order(created_at: "ASC");
     
   end
 
